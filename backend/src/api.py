1c62a80c78b05b7ed0c9e1950db6c115aec70515
Frank Nguyen <frank.nguyen.vd@gmail.com>
2021-05-14 17:28:29 +0800

    🎨 run black to format all files

--- backend/src/api.py
+++ backend/src/api.py
@@ -90,7 +90,10 @@ Example error handling for unprocessable entity
 
 @app.errorhandler(422)
 def unprocessable(error):
-    return jsonify({"success": False, "error": 422, "message": "unprocessable"}), 422
+    return (
+        jsonify({"success": False, "error": 422, "message": "unprocessable"}),
+        422,
+    )
 
 
 """
