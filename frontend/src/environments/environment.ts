a498bc156cc82396874368f2af877f5d27f1a1aa
Frank Nguyen <frank.nguyen.vd@gmail.com>
2021-05-16 15:15:40 +0800

    fix the auth0 domain name in frontend env file

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'franknguyenvd.au.auth0.com', // the auth0 domain prefix
+    url: 'franknguyenvd.au', // the auth0 domain prefix
     audience: 'localhost:5000', // the audience set for the auth0 app
     clientId: 'nDydbCaRbNAultiBLz5p9JxDGRSxhYIH', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
